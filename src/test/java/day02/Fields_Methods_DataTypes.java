cf7c1c16a451e06cdbafbda713f4857b70bf8e55
tugceacar <tugceacar.ozkan@gmail.com>
2023-02-25 11:16:18 -0500

    second time commit

---
+++ src/test/java/day02/Fields_Methods_DataTypes.java
@@ -0,0 +1,29 @@
+package day02;
+
+public class Fields_Methods_DataTypes {
+
+    /*
+    1- Field / Variable
+
+    int number =10;
+
+
+    2- Method / Function
+
+    void number() { }
+
+     */
+
+    // DataType : Primitive and Non-Primitive
+
+    // 8 Primitive data type :
+
+
+
+
+
+
+
+
+
+}
